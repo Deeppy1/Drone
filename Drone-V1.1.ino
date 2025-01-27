6e80db1cfd81a985a775d987322bff9f604bfb0d
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2025-01-27 10:30:06 +0000

    Tried to add Forward motors

--- Drone-V1.1.ino
+++ Drone-V1.1.ino
@@ -4,6 +4,7 @@
 Bluepad32 bp32;
 
 GamepadPtr myGamepad;
+bool arm = False
 
 //Motor Pins
 const int MotorPin1 = 2;
@@ -14,7 +15,6 @@ const int MotorPin4 = 5;
 int VerticalSpeed 
 int LeftSticky
 
-int RightSticky
 // Callback when a gamepad is connected
 void onGamepadConnected(GamepadPtr gp) {
   Serial.println("Gamepad connected!");
@@ -26,7 +26,26 @@ void onGamepadDisconnected(GamepadPtr gp) {
   Serial.println("Gamepad disconnected!");
   myGamepad = nullptr;
 }
+void SetVerticalSpeed(int MotorSpeed){
+    analogWrite(MotorPin1, MotorSpeed);
+    analogWrite(MotorPin2, MotorSpeed);
+    analogWrite(MotorPin3, MotorSpeed);
+    analogWrite(MotorPin4, MotorSpeed);
 
+}
+void SetForwardSpeed(int FMotorSpeed){
+    if (ForwardSpeed < 0) {
+    ForwardSpeed = -ForwardSpeed;
+  } else {
+    ForwardSpeed = ForwardSpeed;
+  }
+    FMotorSpeed = (ForwardSpeed / 3)
+    analogWrite(MotorPin1, MotorSpeed - FMotorSpeed);
+    analogWrite(MotorPin2, MotorSpeed - FMotorSpeed);
+    analogWrite(MotorPin3, MotorSpeed + FMotorSpeed);
+    analogWrite(MotorPin4, MotorSpeed + FMotorSpeed);
+
+}
 void setup() {
   Serial.begin(115200);
 
@@ -35,20 +54,24 @@ void setup() {
 
   // Set up the built-in LED
   pinMode(MotorPin1, OUTPUT);
-  pinMode(MotorPin2, OUTPUT);
-  pinMode(MotorPin3, OUTPUT);
-  pinMode(MotorPin4, OUTPUT);
 }
 
 void loop() {
+
   // Update the Bluepad32 instance
   bp32.update();
 
   // If a gamepad is connected
   if (myGamepad && myGamepad->isConnected()) {
+
+    if (myGamepad->a()) {
+      Serial.println("Button A pressed!");
+      arm = True; // Turn LED on
+    } 
+
     // Get the left stick Y-axis value
-    LeftSticky = myGamepad->axisLY(); // Range: -512 to 512
-    RightSticky = myGamepad->axisRY();
+    LeftSticky = myGamepad->axisY(); // Range: -512 to 512
+
     // Map the stick value to LED VerticalSpeed (0-255)
     VerticalSpeed = map(LeftSticky, -512, 0, 0, 255);
 
@@ -58,20 +81,20 @@ void loop() {
       SetVerticalSpeed(0);
 
     }
-    void SetVerticalSpeed(int MotorSpeed){
-      analogWrite(MotorPin1, MotorSpeed);
-      analogWrite(MotorPin2, MotorSpeed);
-      analogWrite(MotorPin3, MotorSpeed);
-      analogWrite(MotorPin4, MotorSpeed);
 
-    }
     // Set the Motor Vertical Speed
-    Void 
+    RightSticky = myGamepad->axisRY()
+    ForwardSpeed = map(RightSticky, -512, 0 , 255)
 
-    Serial.print("Left Stick Y: ");
-    Serial.print(LeftSticky);
-    Serial.print(" -> Motor Vertical Speed: ");
-    Serial.println(VerticalSpeed);
-    SetVerticalSpeed(LeftSticky);
+
+    if(arm == True){
+      Serial.print("Left Stick Y: ");
+      Serial.print(LeftSticky);
+      Serial.print(" -> Motor Vertical Speed: ");
+      Serial.println(VerticalSpeed);
+      SetVerticalSpeed(LeftSticky);
+    }else(){
+      Serial.println("Drone unarmed")
+    }
   }
 }
