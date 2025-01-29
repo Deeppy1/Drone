3478de2e043f5bbbb19f878321ba69b7ceb3708d
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2025-01-29 21:39:59 +0000

    Update MotorPinTest.ino

--- MotorPinTest.ino
+++ MotorPinTest.ino
@@ -1,11 +1,27 @@
 const int MP1 = 16;
-
+const int pot = 15;
+int potval;
 void setup(){
-    PinMode(MP1, OUTPUT);
+    pinMode(MP1, OUTPUT);
+    pinMode(pot, INPUT);
+    Serial.begin(115200);
+
+  Serial.begin(115200);
+
+  // Setup PWM for motor pins using native LEDC functions
+  ledcSetup(0, 5000, 8); // Channel 0, 5kHz, 8-bit resolution
+  ledcAttachPin(MP1, 0);
 }
+
+void Estop() {
+  digitalWrite(MP1, 0);
+}
+
+
 void loop(){
-    digitalWrite(MP1, HIGH);
-    delay(3000);
-    digitalWrite(MP1, LOW);
-    delay(3000);
+    potval = analogRead(pot);
+    Serial.println(potval);
+    int potval2 = map(potval, 4095, 0, 1000, 0 );
+    ledcWrite(0, potval2);
+    
 }
